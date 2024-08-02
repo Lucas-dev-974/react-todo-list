4ad8c2207cb5d8b682beeb9bfb5e7357eb53d234
lucas.lvn <lucas.lvn97439@gmail.com>
2024-08-02 15:36:20 +0400

    add default button with icon and integrate them to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
+import Button from "@/components/DefaultButton";
+import { PlusIcon } from "@/icons/PlusIcon.";
+import { TrashIcon } from "@/icons/TrashIcon";
 import { useState } from "react";
 
-type TaskListType = {
+export const [taskList, setTaskList] = useState<taskItemType[]>([])
+
+type taskItemType = {
   task: string
 }
 
-export const [taskList, setTaskList] = useState<TaskListType[]>([])
+
+
+function addTaskButton(){}
+function removeTaskButton(){}
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Todo List </h1>
 
-      
+      <Button onClick={addTaskButton} icon={<PlusIcon />} text="" type="append" />
+      <Button onClick={removeTaskButton} icon={<TrashIcon />} text="" type="append" />
     </main>
   );
 }
--- src/components/DefaultButton.tsx
+++ src/components/DefaultButton.tsx
@@ -1,3 +1,14 @@
-export default function Button(){
-    return <button></button>
+import {  ReactNode } from "react";
+
+interface ButtonProps {
+    onClick: () => void;
+    text: string
+    type: "append" | "remove"
+    icon?: ReactNode
+}
+
+export default function Button(props: ButtonProps){
+    const child = props.icon ?? props.text
+
+    return <button className="m-1" onClick={props.onClick}>{child}</button>
 }
