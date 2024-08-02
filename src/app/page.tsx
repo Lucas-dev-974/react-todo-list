24bc1b8bcbee3c46f3b42044a313c5644279660e
lucas.lvn <lucas.lvn97439@gmail.com>
2024-08-02 14:20:34 +0400

    default files

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+type TaskListType = {
+  task: string
+}
+
+export const [taskList, setTaskList] = useState<TaskListType[]>([])
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Todo List </h1>
+
+      
+    </main>
+  );
+}
